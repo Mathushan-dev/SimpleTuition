bbf5a95ecc983cadfbd8f88f187f1ba23e881772
Mathushan Mathiyalagan <mathiyalaganmathushan@gmail.com>
2022-09-04 18:40:21 +0100

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zhveshbh:rsu57TwANLAhIbLcBG_pe-AmnOIXNaAG@surus.db.elephantsql.com/zhveshbh'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Mathushan-dev:v2_3ttBs_2GAqqzGKELEa6jDbLCfbNZP@db.bit.io/Mathushan-dev/simpletuition'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)

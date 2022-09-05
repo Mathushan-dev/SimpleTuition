ee8195d120d2bac46f73772c5be89e33c95da511
Mathushan Mathiyalagan <mathiyalaganmathushan@gmail.com>
2022-09-05 21:27:07 +0100

    Update db.sql

--- application/db.sql
+++ application/db.sql
@@ -17,6 +17,9 @@ CREATE TABLE questions (
 INSERT INTO questions (id, question, keywords, exam_id)
 VALUES (1, 'What is a cell?', '[[["small cells", "tiny cells"], ["cell wall", "cell membrane"]], [["nucleus"], ["DNA", "genetic material"]]]', 1);
 
+INSERT INTO users (username, email_address, password_hash, outstanding_questions, completed_questions, log)
+VALUES ('nirusa98', 'mathiyalaganmathushan@gamil.com', '$2b$12$xd8ApXCvqUAL.v0zhm3HU.UvXCiLpiRPCD6oCAzzJVJhoyiieih26', NULL, NULL, 'Account created on 08/23/2022, 16:52:33');
+
 UPDATE users
 SET outstanding_questions = '1'
 WHERE id = 2;
